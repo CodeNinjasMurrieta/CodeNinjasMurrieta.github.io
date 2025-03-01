9bc09b17254c7871c2eac71250a0297c1f85b1a8
CodeNinjasMurrieta <126828767+CodeNinjasMurrieta@users.noreply.github.com>
2025-02-28 17:40:19 -0800

    Added sprites files and folders

---
+++ sprites.js
@@ -0,0 +1,406 @@
+var ninja_list = [
+				{ name: "Sensei R", width: 16, height: 16, sprite: 	
+						`img\`
+						. . . . . . . . . . b 7 b . . . 
+						. . . . . . . . . b 7 b . . . . 
+						. . . . . . b b b b b b . . . . 
+						. . . . . b b 7 7 7 7 7 2 2 . . 
+						. . . . b b 7 2 2 2 7 7 2 f . . 
+						. . . . b 7 7 2 2 f 7 7 4 2 . . 
+						. . . . b 7 7 2 2 2 7 7 4 4 . . 
+						. b b b 7 7 7 7 7 7 4 4 4 4 4 b 
+						b 7 7 7 b b 7 7 7 4 4 4 4 4 b . 
+						b b 7 7 7 7 b 7 7 1 1 7 1 2 . . 
+						c 7 c 7 7 7 7 d 7 2 1 7 1 7 b . 
+						c 7 7 c 7 7 7 b 7 7 2 7 7 7 b . 
+						. c 7 7 c c b 7 7 7 7 7 7 d b . 
+						. . c 7 7 7 7 7 7 7 7 7 b b . . 
+						. . . c c c c c c c c b b . . . 
+						. . . . . . . . . . . . . . . . 
+						\``
+												
+				},
+				
+				{ name: "Adrian C", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Adrian M", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Aidan V", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Ailsa L", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Anayah P", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Anthony S", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Arham P", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Asher C", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Atharva S", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Benjamin B", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Bryant W", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Cain D", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Calan S", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Caleb L", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Cameron C", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Camila V", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Carey Z", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Carlos G", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Charlie E", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Chelsea W", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Christopher D", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Damian O", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "David H", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Eddy C", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Eli M", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Eli V", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Elijah B", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Elijah C", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Elijah L", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Elliot S", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Gabrielle D", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Guillermo N", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Hendrix M", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Hudson B", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Hudson S", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Hunter K", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Isaac V", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Isabella R", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Isaiah R", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Ivy W", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Jack Welsh", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Jacobo Z", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "James G", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Jaxon K", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Jaxson K", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Jaylin T", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Jeremy J", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Jimmy B", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Judah H", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Kaz B", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Keren B", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Killian F", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Lauren H", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Lennox R", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Leo O", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Levi P", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Logan F", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Luke Q", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Magnus D", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Manuel L", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Marcus Z", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Mateo V", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Matthew O", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Max M", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Maximus M", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Maxson L", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Mayukhi Y", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Mercurius M", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Micah D", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Nehemiah S", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Nicholas F", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Nikko J", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Oliver R", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Oliver T", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Oscar D", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Owen B", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Preston F", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Raine C", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Ryan M", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Ryker M", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Rylee C", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Samuel S", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Seamus W", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Sky P", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Sofia Z", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Takeo D", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Titus W", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Vincent L", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Vivaan M", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Viviana M", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Will B", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "William C", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "William H", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Yahel H", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Yaseen K", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Yohan P", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Yusuf K", width: 0, height: 0, sprite: 	
+						`None` 
+				},
+				{ name: "Zion B", width: 0, height: 0, sprite: 	
+						`None` 
+				}
+			]
+			
+			
+			
+			function createDiv(index) {
+				var div = document.createElement("div");
+			  
+				div.style.width = "180px";
+				div.style.height = "250px";
+				div.style.border = "medium solid black";
+				div.style.color = "black";
+				div.style.fontSize = "25px";
+				div.style.display = "inline-grid";
+				div.style.padding = "10px";
+				div.style.margin = "10px";
+				div.style.textAlign = "center";
+				
+				var name = document.createElement("h1");
+				name.innerText = ninja_list[index].name + ".";
+				
+				
+				var image = document.createElement("img");				
+				image.src = "./SpriteImages/" + ninja_list[index].name + "/Capture.PNG";
+				image.style.marginRight = "auto";
+				image.style.marginLeft = "auto";
+				
+				
+				//Set image size based on dimensions
+				if (ninja_list[index].width > ninja_list[index].height) {
+					image.setAttribute("width", div.style.width);
+				}
+				else {
+					image.setAttribute("height", "150px");
+				}
+				var dimensions = document.createElement("h2");
+				if (ninja_list[index].width > 0) {
+					dimensions.innerText = ninja_list[index].width + " x " + ninja_list[index].height;
+				}
+				
+				
+				var button = document.createElement("button");
+				button.innerText = "Copy";
+				
+				if (ninja_list[index].width === 0) {
+					button.disabled = true;
+				}
+			
+				
+				
+				button.addEventListener("click", (event) => {
+					navigator.clipboard.writeText(ninja_list[index].sprite);
+				});		
+			
+				div.appendChild(document.createTextNode(name.innerText));
+				div.appendChild(image);
+				div.appendChild(document.createTextNode(dimensions.innerText));
+				div.appendChild(button);
+				
+				return div;
+			}
+			
+			//Need Child Name, Sprite Image, Sprite Code, and Sprite Dimensions
+			var divs = []
+			for (var i = 0; i < ninja_list.length; i++) {
+				divs.push(createDiv(i));
+			}
+			
+			/**
+			var divs = [
+			  createDiv(0),
+			  createDiv(1),
+			  createDiv(0),
+			  createDiv(1),
+			  createDiv(0),
+			  createDiv(1),
+			  createDiv(0),
+			  createDiv(1)
+			  //createDiv("baz")
+			];
+			*/
+
+			var docFrag = document.createDocumentFragment();
+			for(var i = 0; i < divs.length; i++) {
+			  docFrag.appendChild(divs[i]); // Note that this does NOT go to the DOM
+			}
+
+			document.getElementById("sprite_area").appendChild(docFrag); // Appends all divs at once
+			
+			
